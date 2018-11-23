baaffe0fecee2f06891591d56cb145dad44d2ff6
victorbotti <vicbotce@inf.upv.es>
2018-11-23 09:53:04 +0100

    http funciona mas o menos

--- src/navssr/FXMLDocument_MainController.java
+++ src/navssr/FXMLDocument_MainController.java
@@ -5,12 +5,10 @@
  */
 package navssr;
 
-
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
 import java.net.URL;
 import java.net.URLConnection;
@@ -23,13 +21,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  *
  * @author sirkora
  */
 public class FXMLDocument_MainController implements Initializable {
-    
+
     @FXML
     private Label label;
     @FXML
@@ -38,48 +37,40 @@ public class FXMLDocument_MainController implements Initializable {
     private WebView panelWEB;
     @FXML
     private TextField paginaURL;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        String pagina = "";
-        String paginaURLString = paginaURL.getText();
-        
-        System.out.println(paginaURLString);
-        label.setText("Hello World!");
-        URL url;
+        label.setText("Le has dado a buscar");
+        String userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
+        String url = "https://" + paginaURL.getText();
         try {
-            final WebEngine webEngine = panelWEB.getEngine();
-            //webEngine.load("http://www.pccomponentes.es");
-            // Creando un objeto URL
-            
-            url = new URL("http://" + paginaURLString);
- 
-            // Realizando la petición GET
-            URLConnection con = url.openConnection();
- 
-            // Leyendo el resultado
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
- 
-            String linea;
-            while ((linea = in.readLine()) != null) {
-                pagina += linea;
+            URL obj = new URL(url);
+            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("user-agent", userAgent);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            String response = new String();
+            while ((inputLine = in.readLine()) != null) {
+                response += inputLine;
             }
-            System.out.println(pagina);
-            webEngine.loadContent(pagina);
+            in.close();
+            System.out.print(response);
+            WebEngine webEngine = panelWEB.getEngine();
             
-            
-            
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+            webEngine.loadContent( response, "text/html");
+              
+
+        } catch (Exception e) {
+            System.out.println(e);
         }
- 
-    
+
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
