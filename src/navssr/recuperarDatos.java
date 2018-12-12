d6cafb7ac106c1adc9c3b34c38f8830599130896
victorbotti <vicbotce@inf.upv.es>
2018-12-12 17:16:40 +0100

    descargar pagina y javascript

--- src/navssr/recuperarDatos.java
+++ src/navssr/recuperarDatos.java
@@ -7,6 +7,7 @@ package navssr;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,30 +29,30 @@ import org.jsoup.select.Elements;
  * @author victorbotti
  */
 public class recuperarDatos {
-    
+
     private String webResponse;
     private String webURL;
-    
-    public recuperarDatos(String webURL){
+
+    public recuperarDatos(String webURL) {
         this.webResponse = recuperarPagina(webURL);
         this.webURL = webURL;
     }
-    
-    public String recuperarPagina(String link){
+
+    private String recuperarPagina(String link) {
         URL url;
         String pagina = "";
         try {
-            
+
             // Creando un objeto URL
             url = new URL(link);
- 
+
             // Realizando la petición GET
             HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
             con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
             // Leyendo el resultado
             BufferedReader in = new BufferedReader(new InputStreamReader(
                     con.getInputStream()));
- 
+
             String linea;
             while ((linea = in.readLine()) != null) {
                 pagina += linea + "\n";
@@ -61,32 +62,101 @@ public class recuperarDatos {
         }
         return pagina;
     }
-    
-    public void recuperarImagenes() throws MalformedURLException, IOException{
+
+    public void recuperarImagenes() throws MalformedURLException, IOException {
         Document doc = Jsoup.parse(this.webResponse);
-            Elements links = doc.select("img");
-            int cont=0;
-            String[] pag = webURL.split("\\.");
+        Elements links = doc.select("img");
+        String[] pag = webURL.split("\\.");
+
+        try {
+            new File("../DatosPaginas/" + pag[1] + "/imagenes").mkdirs();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        for (Element lin : links) {
+            String href = lin.attr("src");
+            if (href.substring(0, 2).equals("//")) {
+                href = "https:" + href;
+            } else if (href.substring(0, 1).equals("/")) {
+                href = webURL + href;
+            }
+            String[] linkDiv = href.split("/");
+            try {
+                URL urlImagen = new URL(href);
+                HttpsURLConnection con = (HttpsURLConnection) urlImagen.openConnection();
+                con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+                InputStream in = con.getInputStream();
+                Files.copy(in, Paths.get("../DatosPaginas/" + pag[1] + "/imagenes/" + linkDiv[linkDiv.length - 1]));
+            } catch (FileAlreadyExistsException e) {
+            }
             
-            try{
-            new File("../DatosPaginas/" + pag[1]+"/imagenes").mkdirs();
-            }catch(Exception e){System.out.println(e);}
-            
-            for(Element lin : links){
-                String href = lin.attr("src");
-                if(href.substring(0,2).equals("//")){href = "https:"+ href; }
-                else if(href.substring(0,1).equals("/")){href = webURL + href;}
-                String[] linkDiv = href.split("/");
-                try{
-                    URL urlImagen = new URL(href);
-                    HttpsURLConnection con = (HttpsURLConnection) urlImagen.openConnection();
-                    con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
-                    InputStream in = con.getInputStream();
-                    Files.copy(in, Paths.get("../DatosPaginas/"+pag[1] +"/imagenes/"+ linkDiv[linkDiv.length-1]));
-                }catch(FileAlreadyExistsException e){}
-        System.out.println(href);
+        }
     }
-    
-    
+
+    public void recuperarHTML() {
+        FileWriter fichero = null;
+        String[] pag = this.webURL.split("\\.");
+        try {
+            fichero = new FileWriter("../DatosPaginas/" + pag[1] + "/index.html");
+
+            // Escribimos linea a linea en el fichero
+            fichero.write(this.webResponse);
+
+            fichero.close();
+
+        } catch (Exception ex) {
+            System.out.println("Mensaje de la excepción: " + ex.getMessage());
+        }
+    }
+
+    public void recuperarJavaScript() {
+        Document doc = Jsoup.parse(this.webResponse);
+        Elements links = doc.select("script");
+  
+
+        for (Element lin : links) {
+
+            String href = lin.attr("src");
+            if(href == ""){continue;}
+           
+            if (href.substring(0, 2).equals("//")) {
+                href = "https:" + href;
+            } else if (href.substring(0, 1).equals("/")) {
+                href = webURL + href;
+            }
+            System.out.println(href);
+            try {
+                // Creando un objeto URL
+                URL url = new URL(href);
+
+                // Realizando la petición GET
+                HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+                con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+                // Leyendo el resultado
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+
+                String linea;
+                String pagina = "";
+                while ((linea = in.readLine()) != null) {
+                    pagina += linea + "\n";
+                }
+                String[] pag = this.webURL.split("\\.");
+                String[] linkDiv = href.split("/");
+                File fichero = new File("../DatosPaginas/" + pag[1] + "/javascript/"+linkDiv[linkDiv.length -1]);
+                fichero.getParentFile().mkdirs();
+                FileWriter write = new FileWriter(fichero);
+
+                // Escribimos linea a linea en el fichero
+                write.write(pagina);
+
+                write.close();
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
     }
 }
